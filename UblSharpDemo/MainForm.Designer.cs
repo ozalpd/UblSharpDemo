78ee6fda1a2dba6f144aae4bf7c1dbde370fff74
Özalp Döndüren <ozalp@donduren.com>
2022-09-04 16:42:08 +0300

    Initial commit

---
+++ UblSharpDemo/MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace UblSharpDemo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDirPath = new System.Windows.Forms.TextBox();
+            this.btnListFiles = new System.Windows.Forms.Button();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtDirPath
+            // 
+            this.txtDirPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDirPath.Location = new System.Drawing.Point(104, 12);
+            this.txtDirPath.Name = "txtDirPath";
+            this.txtDirPath.Size = new System.Drawing.Size(485, 23);
+            this.txtDirPath.TabIndex = 0;
+            // 
+            // btnListFiles
+            // 
+            this.btnListFiles.Location = new System.Drawing.Point(12, 12);
+            this.btnListFiles.Name = "btnListFiles";
+            this.btnListFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnListFiles.TabIndex = 1;
+            this.btnListFiles.Text = "List Files";
+            this.btnListFiles.UseVisualStyleBackColor = true;
+            this.btnListFiles.Click += new System.EventHandler(this.btnListFiles_Click);
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.ItemHeight = 15;
+            this.lstFiles.Location = new System.Drawing.Point(12, 41);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(577, 199);
+            this.lstFiles.TabIndex = 2;
+            this.lstFiles.SelectedIndexChanged += new System.EventHandler(this.lstFiles_SelectedIndexChanged);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtStatus.Location = new System.Drawing.Point(12, 246);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ReadOnly = true;
+            this.txtStatus.Size = new System.Drawing.Size(577, 192);
+            this.txtStatus.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 450);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.btnListFiles);
+            this.Controls.Add(this.txtDirPath);
+            this.Name = "MainForm";
+            this.Text = "OASIS UBL Invoice Reader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtDirPath;
+        private Button btnListFiles;
+        private ListBox lstFiles;
+        private TextBox txtStatus;
+    }
+}
---
+++ UblSharpDemo/MainForm.cs
@@ -0,0 +1,103 @@
+﻿using System.Xml.Serialization;
+using UblSharp;
+using UblSharp.CommonAggregateComponents;
+
+namespace UblSharpDemo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListFiles_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtDirPath.Text))
+            {
+                MessageBox.Show("A directory path is needed to list some XML files!",
+                                "Empty Directory Path",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            var files = Directory.GetFiles(txtDirPath.Text, "*.xml");
+            lstFiles.Items.Clear();
+            lstFiles.Items.AddRange(files);
+        }
+
+        private void lstFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstFiles.Items.Count == 0)
+                return;
+
+            var selectedFile = lstFiles.SelectedItem as string ?? lstFiles.Items[0].ToString();
+            if (string.IsNullOrEmpty(selectedFile))
+                return;
+
+            var invoice = OpenInvoice(selectedFile);
+            if (invoice != null)
+                ReadInvoice(invoice);
+        }
+
+        public InvoiceType? OpenInvoice(string filePath)
+        {
+            InvoiceType? invoice = null;
+            XmlSerializer serializer = new XmlSerializer(typeof(InvoiceType));
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                invoice = (InvoiceType?)serializer.Deserialize(reader);
+            }
+
+            return invoice;
+        }
+
+        public void ReadInvoice(InvoiceType invoice)
+        {
+            var invoiceId = invoice.ID;
+            var lines = invoice.InvoiceLine;
+            var currencyCode = invoice.DocumentCurrencyCode;
+
+
+            var isseuDate = invoice.IssueDate.Value.ToString("dd.MM.yyyy");
+            var biller = invoice.AccountingSupplierParty.Party;
+            var billerIds = biller.PartyIdentification;
+            var taxNr = billerIds.FirstOrDefault(pi => pi.ID.schemeID.Equals("VKN"));
+            if (taxNr == null)
+            {
+                taxNr = billerIds.FirstOrDefault(pi => pi.ID.schemeID.Equals("TCKN"));
+            }
+
+            var agent = biller.AgentParty;
+            var uuid = invoice.UUID.Value;
+
+            txtStatus.Text = $"Tax Nr: {taxNr.ID.Value} \r\nIssue Date: {isseuDate} \r\nUUID: {uuid} ";
+            var taxes = invoice.TaxTotal;
+
+            foreach (var tax in taxes)
+            {
+                DisplayTax(tax);
+            }
+        }
+
+        private void DisplayTax(TaxTotalType? tax)
+        {
+            txtStatus.Text += $"\r\nTotal Tax: {tax.TaxAmount.Value} {tax.TaxAmount.currencyID}";
+            foreach (var item in tax.TaxSubtotal)
+            {
+                DisplayTax(item);
+            }
+        }
+
+        private void DisplayTax(TaxSubtotalType tax)
+        {
+            var taxAmount = tax.TaxAmount.Value;
+            var taxableAmount = tax.TaxableAmount.Value;
+            var taxRate = tax.Percent.Value;
+
+            txtStatus.Text += $"\r\nTax {taxRate}% {taxAmount} {tax.TaxAmount.currencyID}";
+            txtStatus.Text += $",  Taxable {taxRate}: {taxableAmount} {tax.TaxAmount.currencyID}";
+        }
+    }
+}
